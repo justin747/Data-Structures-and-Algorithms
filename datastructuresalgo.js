36f1b7cbef36fa885ee24cabf2dd4af5b5638ee9
justin747 <justin747@gmail.com>
2022-12-28 23:56:57 -0800

    Fixed bug
    Previous Bubble Sort was an infinite loop. Add a condition to exit when all swaps are complete

--- datastructuresalgo.js
+++ datastructuresalgo.js
@@ -344,17 +344,41 @@ function naiveSearch(long, short){
     return count;
 }
 
-//Colt Steele - Bubble Sort ES6
+//Colt Steele - Bubble Sort ES5
 
 function bubbleSort(arr){
+    var noSwaps;
+
     for(var i = arr.length; i > 0; i--){
+        noSwaps = true;
         for(var j = 0; j < i - 1; j++){
             if(arr[j] > arr[j+1]){
                 var temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                noSwaps = false;
+            }
+        }
+
+        if(noSwaps) break;
+    }
+    return arr;
+}
+
+//Colt Steele - Bubble Sort ES6
+
+function bubbleSortES6(arr){
+    const swap = (arr, index1, index2) => {
+        [arr[index1], arr[index2] = arr[index2], arr[index1]];
+    };
+
+    for(let i = arr.length; i > 0; i--){
+        for(let j = 0; j< i - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                swap(arr, j, j + 1);
             }
         }
     }
+
     return arr;
 }
