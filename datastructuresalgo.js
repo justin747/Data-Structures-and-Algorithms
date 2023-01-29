0ef4d2ae3139beb98df3b3853ee7b58fd2d17c0a
justin747 <justin747@gmail.com>
2023-01-28 16:46:23 -0800

    Binary Search
    Wrote a function called binarySearch which accepts a sorted array and a value and returns the index at which the value exists. Otherwise, return -1.

--- datastructuresalgo.js
+++ datastructuresalgo.js
@@ -303,3 +303,25 @@ function linearSearch(arr, val){
         }
     }
 }
+
+//Colt Steele - Binary Search Problem
+
+function binarySearch(arr, val){
+    var start = 0;
+    var end = arr.length - 1;
+    var middle = Math.floor((start + end) / 2);
+
+    while(arr[middle] !== val && start <= end){
+        if(val < arr[middle]){
+            end = middle - 1;
+        } else {
+            start = middle + 1;
+        }
+        middle = Math.floor((start + end) / 2);
+    }
+    if(arr[middle] === val){
+        return middle;
+    } else {
+        return -1;
+    }
+}
