e4df86265024944b39063c6b9f9a6e3e0b83042e
justin747 <justin747@gmail.com>
2023-01-30 13:00:19 -0800

    Bubble Sort
    Wrote a function called Bubble Sort that takes an unordered array, loops through the array and compares a value to the next index in the array
    
    If the first index is higher the second index, the values are swapped. This is repeated via loop to achieve an ordered

--- datastructuresalgo.js
+++ datastructuresalgo.js
@@ -326,7 +326,7 @@ function binarySearch(arr, val){
     }
 }
 
-//Colt Steele Naive String Search
+//Colt Steele - Naive String Search
 
 function naiveSearch(long, short){
     var count = 0;
@@ -342,4 +342,19 @@ function naiveSearch(long, short){
         }
     }
     return count;
+}
+
+//Colt Steele - Bubble Sort ES6
+
+function bubbleSort(arr){
+    for(var i = arr.length; i > 0; i--){
+        for(var j = 0; j < i - 1; j++){
+            if(arr[j] > arr[j+1]){
+                var temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
 }
