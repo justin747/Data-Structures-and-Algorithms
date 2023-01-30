db925cdec3a919460c28c2c58ff7e4cf5ab22f5d
justin747 <justin747@gmail.com>
2023-01-29 17:42:23 -0800

    Naive String Search
    Wrote a function called naiveSearch that counts the number of times a smaller string appears in a longer string.

--- datastructuresalgo.js
+++ datastructuresalgo.js
@@ -325,3 +325,21 @@ function binarySearch(arr, val){
         return -1;
     }
 }
+
+//Colt Steele Naive String Search
+
+function naiveSearch(long, short){
+    var count = 0;
+
+    for(var i = 0; i < long.length; i++){
+        for(var j = 0; j < short.length; j++){
+            if(short[j] !== long[i+j]){
+                break;
+            }
+            if(j === short.length - 1){
+                count++;
+            }
+        }
+    }
+    return count;
+}
