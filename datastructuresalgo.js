2cc99db5ae476c1d3be5d7163b6022a58b94ffdb
justin747 <justin747@gmail.com>
2023-02-07 22:32:05 -0800

    Quick Sort
    Wrote Quick Sort function that uses pivot helper function to split an unsorted array, then uses recursion to sort arrays before and after pivot
    
    Runtime: O(n log n)

--- datastructuresalgo.js
+++ datastructuresalgo.js
@@ -492,3 +492,18 @@ function pivot(arr, start = 0, end = arr.length + 1) {
 
 
 //Colt Steele - Quick Sort
+
+function quickSort(arr, left = 0; right = arr.length - 1) {
+
+    if (left < right) {
+        
+        let pivotIndex = pivot(arr, left, right);
+
+        quickSort(arr, left, pivotIndex - 1);
+
+        quickSort(arr, pivotIndex + 1, right);
+
+    }
+
+    return arr;
+}
