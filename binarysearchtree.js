6c5b7e2892ece14d84651198200104b17ce38ddc
justin747 <justin747@gmail.com>
2023-03-03 22:16:18 -0800

    Binary Search Tree - Breadth First Search
    BFS - Implemented BFS to traverse a BST and create and return an Array with the Nodes of the BST

--- binarysearchtree.js
+++ binarysearchtree.js
@@ -39,20 +39,37 @@ class BST {
     }
 
     contains(value) {
-        if(this.root === null) return false;
+        if (this.root === null) return false;
 
         let current = this.root;
         let found = false;
 
-        while(current && !found) {
-            if(value < current.value) {
+        while (current && !found) {
+            if (value < current.value) {
                 current = current.left;
-            } else if(value > current.value) {
+            } else if (value > current.value) {
                 current = current.right;
             } else {
                 return true;
             }
         }
         return false;
+    }
+
+    BFS() {
+
+        let data = [];
+        let queue = [];
+        let node = this.root;
+
+        queue.push(node);
+
+        while (queue.length) {
+            node = queue.shift
+            data.push(node.value);
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+        return data;
     }
 }
