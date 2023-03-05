17cd9af124f5820c0f19048888aa11e616d39f60
justin747 <justin747@gmail.com>
2023-03-05 00:23:54 -0800

    Binary Search Tree - Depth First Search (PostOrder & InOrder)
    DFS - Implemented DFS (PostOrder & InOrder) to traverse a BST vertically, and create and return an Array with the Nodes of the BST
    
    PostOrder - PreOrder traverses vertically to the left of the BST, then to the right of the BST, and returns an Array sorted from the bottom of the BST to the top
    
    InOrder - traverses vertically to the left of the BST, then to the right of the BST, and returns an Array sorted from the bottom of the BST to the top and returns an Array sorted by value

--- binarysearchtree.js
+++ binarysearchtree.js
@@ -85,4 +85,28 @@ class BST {
         traverse(current);
         return data;
     }
+
+    DFSPostOrder() {
+        let data = [];
+
+        function traverse(node) {
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+            data.push(node);
+        }
+        traverse(this.root);
+        return data;
+    }
+
+    DFSInOrder() {
+        let data = [];
+
+        function traverse(node) {
+            if (node.left) traverse(node.left);
+            data.push(node);
+            if (node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
 }
