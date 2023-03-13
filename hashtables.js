6383bf99bde4208b6d66914af46a3d0469133904
justin747 <justin747@gmail.com>
2023-03-12 22:10:47 -0700

    Hash Table - HashTable class
    Created 1st HashTable class

--- hashtables.js
+++ hashtables.js
@@ -9,3 +9,22 @@ function hash(key, arrayLen) {
     }
     return total;
 }
+
+class HashTable {
+    constructor(size = 53) {
+        this.keyMap = new Array(size);
+    }
+
+    _hash(key) {
+        let total = 0;
+        let WEIRD_PRIME = 31;
+
+        for (let i = 0; i< Math.min(key.length, 00); i++) {
+            let char = key[i];
+            let value = char.charCodeAt(0) - 96
+            total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+        }
+
+        return total;
+    }
+}
