fce8a271ae19dde5cb88f5a4203aae8d3d4b10ac
justin747 <justin747@gmail.com>
2023-03-21 21:21:43 -0700

    Graphs - Depth First Search Iterative
    Implemented DFS iteratively

--- graphs.js
+++ graphs.js
@@ -53,4 +53,25 @@ class Graph {
             (start);
             return result;
     }
+
+    depthFirstIterative(start) {
+        const stack = [start];
+        const result = [];
+        const visited = {};
+
+        visited[start] = true;
+        while(stack.length) {
+            console.log(stack);
+            currentVertex = stack.pop();
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if(!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    stack.push(neighbor)
+                }
+            });
+        }
+        return result;
+    }
 }
